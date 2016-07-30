a8f14891c3bb03e53644a7d533a9a9a2c6b86dad
cj.gomlop@gmail.com <cj.gomlop@gmail.com>
2016-07-30 21:15:21 +0200

    directorio public funciones generales

--- core/core.php
+++ core/core.php
@@ -6,6 +6,7 @@
 define('HTML_DIR', 'html/');
 define('APP_TITLE', 'OcrenndBB');
 define('APP_COPY', 'Copryright &copy;' .date('Y', time()) . ' OcrendBB Software. ');
+define('APP_URL', 'http://localhost:8088/Proyectos/prinickPHP/OcrendBB/');
 
 require('vendor/autoload.php');
 //require ('PHPMailerAutoload.php');
---
+++ views/app/js/generales.js
@@ -0,0 +1,4 @@
+// funciones generales
+function __(id) {
+  return document.getElementById(d);
+}
